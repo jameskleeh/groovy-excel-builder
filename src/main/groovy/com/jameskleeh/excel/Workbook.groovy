b275363a1ffff57c45152342415eef1c14e9a0d3
jameskleeh <james.kleeh@gmail.com>
2016-10-07 19:43:53 -0400

    Cleanup

--- src/main/groovy/com/jameskleeh/excel/Workbook.groovy
+++ src/main/groovy/com/jameskleeh/excel/Workbook.groovy
@@ -4,13 +4,13 @@ or more contributor license agreements.  See the NOTICE file
 distributed with this work for additional information
 regarding copyright ownership.  The ASF licenses this file
 to you under the Apache License, Version 2.0 (the
-        "License"); you may not use this file except in compliance
+"License"); you may not use this file except in compliance
 with the License.  You may obtain a copy of the License at
 
-http://www.apache.org/licenses/LICENSE-2.0
+  http://www.apache.org/licenses/LICENSE-2.0
 
 Unless required by applicable law or agreed to in writing,
-        software distributed under the License is distributed on an
+software distributed under the License is distributed on an
 "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
 KIND, either express or implied.  See the License for the
 specific language governing permissions and limitations
@@ -34,7 +34,6 @@ class Workbook {
 
     private final XSSFWorkbook wb
     private final CellStyleBuilder styleBuilder
-
 
     private static final String WIDTH = 'width'
     private static final String HEIGHT = 'height'
